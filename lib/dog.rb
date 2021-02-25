197acced9785c75d12cbeb007d06bf46157afcce
George Serna <sernageorge@gmail.com>
2021-02-25 02:17:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,21 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
 
   def name=(name)
     @name=name
   end
 
+  def breed=(breed = "Mutt")
+    @breed = breed
+  end
+
   def name
     @name
   end
 
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed=breed
-  end
   def breed
     @breed
   end
